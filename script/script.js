142152b6fdbc47fba947c93df3225b39139741ae
zastar23 <florin.porut@gmail.com>
2022-04-05 09:53:19 +0300

    Mobile complete.
    Took me some time but it is for the first time that I reproduced a pixel perfect copy of the given design.

---
+++ script/script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+const blob = document.querySelector(".color-blob");
+const navList = document.querySelector(".nav-list");
+
+btn.addEventListener("click", (e) => {
+	if (nav.style.display === "none") {
+		nav.style.display = "block";
+		blob.classList.add("dark");
+		navList.classList.add("drop-shadow");
+	} else {
+		nav.style.display = "none";
+		blob.classList.remove("dark");
+		navList.classList.remove("drop-shadow");
+	}
+});
